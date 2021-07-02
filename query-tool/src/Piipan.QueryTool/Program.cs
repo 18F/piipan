a94733851f63a25e62c5dec3e492e89ec816d113
Ryan Hofschneider <ryan.hofschneider@gsa.gov>
2021-07-02 07:56:01 -0700

    Hack to get Azure logging for App Service working under Windows

--- query-tool/src/Piipan.QueryTool/Program.cs
+++ query-tool/src/Piipan.QueryTool/Program.cs
@@ -6,6 +6,9 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging.AzureAppServices;
+
 
 namespace Piipan.QueryTool
 {
@@ -18,6 +21,22 @@ namespace Piipan.QueryTool
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureLogging(logging =>
+                {
+                    logging.ClearProviders();
+                    logging.AddAzureWebAppDiagnostics();
+                })
+                .ConfigureServices(serviceCollection => serviceCollection
+                    .Configure<AzureFileLoggerOptions>(options =>
+                    {
+                        options.FileName = "azure-diagnostics-";
+                        options.FileSizeLimit = 50 * 1024;
+                        options.RetainedFileCountLimit = 5;
+                    }).Configure<AzureBlobLoggerOptions>(options =>
+                    {
+                        options.BlobName = "log.txt";
+                    })
+                )
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
